c2cb4453f85f31f612fb67868ed9426bf5c0420b
Adrian Jamrozik <yamroz@gmail.com>
2021-07-15 20:24:20 +0200

    Moving to other machine.

--- src/test/test_yamconwaylib.py
+++ src/test/test_yamconwaylib.py
@@ -1,24 +1,21 @@
 import pytest
-from yamconwaylib import YamConway
+from yamconway.yamconwaylib import *
 
 
 @pytest.fixture
 def yamconway_default():
     yc = YamConway()
     yield yc
-    print("Finished fixture.")
 
 
-def test_yamconway_boards_exit(yamconway_default):
+def test_yamconway_boards_exist(yamconway_default):
     assert yamconway_default.board1
     assert yamconway_default.board2
-
-
-def test_yamconway_boards_size(yamconway_default):
-    assert len(yamconway_default.board1) == 20
-    assert len(yamconway_default.board1) == 20
 
 
 def test_yamconway_stats_zero(yamconway_default):
     assert yamconway_default.stats.births == 0
     assert yamconway_default.stats.deaths == 0
+
+def test_yamconway_load_board(yamconway_default):
+    yamconway_default.load_board()
