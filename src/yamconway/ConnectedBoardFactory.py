9e88dad06d47a44bb0c4769fd996c36fdb1f3755
Adrian Jamrozik <yamroz@gmail.com>
2021-11-28 14:54:54 +0100

    Time to cleanup file access methods those are all over the place!

--- src/yamconway/ConnectedBoardFactory.py
+++ src/yamconway/ConnectedBoardFactory.py
@@ -15,19 +15,3 @@ class ConnectedBoardFactory:
         connBoard = ConnectedBoard()
         YamConnBoardIO.load_from_file(connBoard,path_to_file)
         return connBoard
-
-    @staticmethod
-    def loadFromFileOld(path_to_file:str) -> ConnectedBoard:
-        newBoard = ConnectedBoardFactory.getConnectedBoard()
-        with open(path_to_file) as specimen:
-            dead_marker = specimen.readline().rstrip()
-            data = specimen.readlines()
-            newBoard.rows_no = len(data)
-            newBoard.cells_in_row = len(data[0].rstrip())
-            #newBoard._init_cells(randomize=False)
-            newBoard._make_cells(randomize=False)
-            for row_idx, row in enumerate(data):
-                for char_idx, character in enumerate(row.rstrip()):
-                    newBoard.cells[row_idx][char_idx].setAlive(
-                        character != dead_marker)
-        return newBoard
